364ef1496ae67e43fd212859865a1fa758162589
Tushar pal <145789688+Tushar0777@users.noreply.github.com>
2025-05-03 14:19:52 +0530

    Update Min Domino Rotation Fix.cpp

--- APRIL_2025/Min Domino Rotation Fix.cpp
+++ APRIL_2025/Min Domino Rotation Fix.cpp
@@ -3,3 +3,34 @@
 
 // similarly for bottom we do the same 
 
+class Solution {
+public:
+    int check(int x,vector<int>& tops, vector<int>& bottoms){
+        int topcount=0;
+        int bottomcount=0;
+
+        for(int i=0;i<tops.size();i++){
+            if(tops[i]!=x && bottoms[i]!=x){
+                return -1;
+
+            }
+            if(tops[i]!=x){
+                bottomcount++;
+            }
+            if(bottoms[i]!=x){
+                topcount++;
+            }
+        }
+        return(min(topcount,bottomcount));
+    }
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+      int tp=check(tops[0],tops,bottoms);
+        if(tp!=-1){
+            return tp;
+        }
+        else{
+            return check(bottoms[0],tops,bottoms);
+        }
+    }
+};
+
