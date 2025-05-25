3baf68b44a5fa2a88e3550e78ea9d38af03b0155
Tushar pal <145789688+Tushar0777@users.noreply.github.com>
2025-05-26 01:01:16 +0530

    Update palindrome pairs.cpp

--- MAY_25/palindrome pairs.cpp
+++ MAY_25/palindrome pairs.cpp
@@ -40,3 +40,82 @@ public:
         
     }
 };
+
+
+
+
+// trie wala implementation 
+
+
+
+class Solution {
+public:
+    bool isPalindrome(string combined){
+        int left=0;
+        int right=combined.size()-1;
+
+        while(right>=left){
+            if(combined[left]!=combined[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        vector<vector<int>>result;
+
+        unordered_map<string,int>freq;
+
+        int n=words.size();
+
+        for(int i=0;i<n;i++){
+            freq[words[i]]=i;
+        }
+        if (freq.count("")) {
+            int emptyIndex = freq[""];
+            for (int i = 0; i < n; i++) {
+                if (i == emptyIndex) continue;
+                if (isPalindrome(words[i])) {
+                    result.push_back({emptyIndex, i});
+                    // result.push_back({i, emptyIndex});
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            string word=words[i];
+            string rev=word;
+            reverse(rev.begin(),rev.end());
+
+            int len=word.size();
+
+            for(int j=0;j<len;j++){
+                string prefix=word.substr(0,j);
+                string suffix=word.substr(j);
+
+                if(isPalindrome(prefix)){
+                    string revsuffix=suffix;
+                    reverse(revsuffix.begin(),revsuffix.end());
+                    if(freq.count(revsuffix) && freq[revsuffix]!=i){
+                        result.push_back({freq[revsuffix],i});
+                    }
+                }
+                if(j!=len && isPalindrome(suffix)){
+                    string revprefix=prefix;
+                    reverse(revprefix.begin(),revprefix.end());
+
+                    if(freq.count(revprefix)&&freq[revprefix]!=i){
+                        result.push_back({i,freq[revprefix]});
+                    }
+                }
+            }
+
+        }
+        return result;
+        
+    }
+};
+
+
