f0096b168b03aced943057138a49b355469c6abe
Tushar pal <145789688+Tushar0777@users.noreply.github.com>
2025-05-29 23:13:56 +0530

    Update 3372.cpp

--- MAY_25/3372.cpp
+++ MAY_25/3372.cpp
@@ -1,1 +1,53 @@
+class Solution {
+public:
 
+    int dfs(unordered_map<int,vector<int>>&adj,int k,int node,int parent){
+        if(k<0){
+            return 0;
+        }
+        int count=1;
+
+        for(auto ngr:adj[node]){
+            if(ngr!=parent){
+                count+=dfs(adj,k-1,ngr,node);
+            }
+
+        }
+        return count;
+    }
+    vector<int> findist(vector<vector<int>>& edges,int k){
+        unordered_map<int,vector<int>>adj;
+
+           int N=edges.size()+1;
+
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int>result(N);
+
+        for(int i=0;i<N;i++){
+           // result[i]=bfs(adj,k,i,N);
+           result[i]=dfs(adj,k,i,-1);
+        }
+        return result;
+    }
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
+        //n=edges1.size()+1;
+
+        vector<int>tree1=findist(edges1,k);
+        vector<int>tree2=findist(edges2,k-1);
+
+        int maxele=*max_element(tree2.begin(),tree2.end());
+        for(int i=0;i<tree1.size();i++){
+            tree1[i]+=maxele;
+        }
+
+        return tree1;
+        
+    }
+};
